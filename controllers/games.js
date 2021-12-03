73f44087c96a96fa8e6156527d5ef734aa0cd7c6
Dave Ridealgh <davecodingstuff@gmail.com>
2021-12-03 06:28:42 +0000

    remove reference to undeclared var

--- controllers/games.js
+++ controllers/games.js
@@ -1,7 +1,6 @@
 const { selectGames } = require('../models/games');
 
 exports.getGames = (req, res, next) => {
-  console.log(hello);
   const { sort_by, order, min_release_year, max_release_year } = req.query;
   selectGames({ sort_by, order, min_release_year, max_release_year })
     .then((games) => {
